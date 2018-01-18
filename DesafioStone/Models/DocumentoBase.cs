22a8449b4df9ab437c072dd1e2ad220d6e179e49
Johann Lemmermann <johnrj@gmail.com>
2018-01-18 19:18:47 -0200

    Commit inicial

---
+++ DesafioStone/Models/DocumentoBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+
+namespace DesafioStone.Models
+{
+    public abstract class DocumentoBase
+    {
+        public ObjectId _id { get; set; }
+    }
+}
