ce7e6f63f393749c8b0313da34f8fcfc56fde30d
Johann Lemmermann <johnrj@gmail.com>
2018-01-21 20:41:32 -0200

    removendo referencias desnecessárias nas classes.

--- DesafioStone/Controllers/DisponibilidadeController.cs
+++ DesafioStone/Controllers/DisponibilidadeController.cs
@@ -1,11 +1,8 @@
 ﻿using DesafioStone.Models;
 using DesafioStone.Negocio;
-using DesafioStone.Repository;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 
--- DesafioStone/Negocio/DisponibilidadeNegocio.cs
+++ DesafioStone/Negocio/DisponibilidadeNegocio.cs
@@ -4,7 +4,6 @@ using MongoDB.Bson;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 
 namespace DesafioStone.Negocio
 {
